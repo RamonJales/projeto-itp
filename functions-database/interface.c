325421389914cf9ce472082d5bf2f86e6791bbee
MariaPaz04 <mpazmarcato@gmail.com>
2023-11-28 14:47:26 -0300

    added option 7

--- functions-database/interface.c
+++ functions-database/interface.c
@@ -8,6 +8,7 @@ void interface(){
     printf("3 - Criar nova linha na tabela\n");
     printf("4 - Listar os dados de uma tabela existente\n");
     printf("5 - Achar um valor em uma tabela existente\n");
-    printf("6 - Apagar o nome e chave primária de uma tabela\n");
+    printf("6 - Apagar uma tupla de uma tabela\n");
+    printf("7 - Apagar uma tabela\n");
     printf("°---------------------------------------------------°\n");
 }
