384f7f594586b8d6cfaa5237826f0b1247f487ed
MariaPaz04 <mpazmarcato@gmail.com>
2023-11-28 14:52:16 -0300

    removed void interface from main

--- main.c
+++ main.c
@@ -57,9 +57,7 @@ void deleteTable(Database *myDatabase, const char *tableName) {
     int found = 0;
     for (int i = 0; i < myDatabase->numTables; i++) {
         if (strcmp(myDatabase->tables[i]->tableName, tableName) == 0) {
-            // Free the memory allocated for the table
             free(myDatabase->tables[i]);
-            // Shift the remaining tables to fill the gap
             for (int j = i; j < myDatabase->numTables - 1; j++) {
                 myDatabase->tables[j] = myDatabase->tables[j + 1];
             }
@@ -72,19 +70,6 @@ void deleteTable(Database *myDatabase, const char *tableName) {
     if (!found) {
         printf("Tabela '%s' nao encontrada!\n", tableName);
     }
-}
-
-void interface(){
-    printf("°---------------------------------------------------°\n");
-    printf("Escolha a opção: \n");
-    printf("1 - Criar tabela\n");
-    printf("2 - Listar as tabelas existentes\n");
-    printf("3 - Criar nova linha na tabela\n");
-    printf("4 - Listar os dados de uma tabela existente\n");
-    printf("5 - Achar um valor em uma tabela existente\n");
-    printf("6 - Apagar uma tupla de uma tabela\n");
-    printf("7 - Apagar uma tabela\n");
-    printf("°---------------------------------------------------°\n");
 }
 
 int main() {
@@ -107,7 +92,7 @@ int main() {
         printf("Tabela");
         break;
     case (2): //LISTAR TABELAS
-        listTable(myDatabase); // função pra ver as tabelas existentes
+        listTable(myDatabase);
         for (int i = 0; i < myDatabase.numTables; i++) {
             free(myDatabase.tables[i]);
         } 
