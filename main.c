9e2cd0d358b7d844d3933b43f53556d14bbbd3bd
RamonJales <ramonjales123@gmail.com>
2023-11-30 18:51:28 -0300

    main function created

--- main.c
+++ main.c
@@ -2,68 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "sgdb.h"
+#include "interface.h"
+
+#define MAX_PK_NAME 50
+#define MAX_TABLE_NAME 50
 
 int main() {
-    FILE *file;
-
-    char tableName[100] = "test2";
-    char line[100];
-    char auxtableName[100];
-    char primaryKey;
     int op;
-    
+
     interface();
 
     printf("Informe a opção escolhida: ");
     scanf("%d", &op);
 
-    switch (op)
-    {
-    case (1): //CRIAR TABELA
-      /*printf("Informe a quantidade de colunas: ");
-      printf("Informe o tipo(?) da coluna: ");
-      printf("Informe o nome da tabela: ");
-      printf("Informe o nome da tabela: ");
-      printf("Informe o nome da tabela: ");
-        
-      create_table(int colQty, char **colTyp, char **colNames, char pkName[], char tableName[])
-        create_table(len, v1, v2, pk, tname);*/
-        printf("Tabela");
-        break;
-    case (2): //LISTAR TABELAS
-        listTables();
-        break;
-    case (3): 
-        break;
-    case (4)://PRINTAR OS DADOS DE UMA TABELA
-        printf("Informe o nome da tabela: ");
-        scanf("%s", &tableName);
-        for (int i = 0; tableName[i]; i++) {
-            tableName[i] = tolower(tableName[i]);
-        }
-        printDataFromTable(tableName);
-        break;
-    case (5):
-        break;
-    case (6): // APAGAR UMA TUPLA
-        printf("Informe o nome da tabela: ");
-        scanf("%s", tableName);
-        printf("Informe a chave primária da tupla a ser apagada: ");
-        scanf("%s", primaryKey);
-        for (int i = 0; tableName[i]; i++) {
-            tableName[i] = tolower(tableName[i]);
-        }
-        deleteTuple(tableName, &primaryKey);
-        break;
-    case (7): // APAGAR UMA TABELA
-        printf("Informe o nome da tabela a ser apagada: ");
-        scanf("%s", tableName);
-        for (int i = 0; tableName[i]; i++) {
-            tableName[i] = tolower(tableName[i]);
-        }
-        deleteTable(tableName);
-        break;
-    }
+    switch (op) {
+        case (1): //CRIAR TABELA
+            int colQty;
+            char **colNames;
+            char pkName[MAX_PK_NAME];
+            char tableName[MAX_TABLE_NAME];
 
+            interfaceCreateTable(&colQty, colNames, pkName, tableName);      
+
+            create_table(colQty, colNames, pkName, tableName);
+
+            free(colNames);
+
+            break;
+        case (2): //LISTAR TABELAS
+            listTables();
+            break;
+        case (3): 
+            break;
+        case (4)://PRINTAR OS DADOS DE UMA TABELA
+            // printf("Informe o nome da tabela: ");
+            // scanf("%s", &tableName);
+            // for (int i = 0; tableName[i]; i++) {
+            //     tableName[i] = tolower(tableName[i]);
+            // }
+            // printDataFromTable(tableName);
+            // break;
+        case (5):
+            break;
+        case (6): // APAGAR UMA TUPLA
+            // printf("Informe o nome da tabela: ");
+            // scanf("%s", tableName);
+            // printf("Informe a chave primária da tupla a ser apagada: ");
+            // scanf("%s", primaryKey);
+            // for (int i = 0; tableName[i]; i++) {
+            //     tableName[i] = tolower(tableName[i]);
+            // }
+            // deleteTuple(tableName, &primaryKey);
+            // break;
+        case (7): // APAGAR UMA TABELA
+            // printf("Informe o nome da tabela a ser apagada: ");
+            // scanf("%s", tableName);
+            // for (int i = 0; tableName[i]; i++) {
+            //     tableName[i] = tolower(tableName[i]);
+            // }
+            // deleteTable(tableName);
+            // break;
+    }
     return 0;
 }
